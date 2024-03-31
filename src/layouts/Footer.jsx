777ca43ca0acd8617ee50d2bee5830d016b915d2
davidcodes17 <areegbedavid@gmail.com>
2024-03-31 01:44:54 +0100

    added:Email-template-semding

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -7,16 +7,21 @@ import {
   FormLabel,
   Heading,
   Input,
+  Link,
   Text,
   Textarea,
 } from "@chakra-ui/react";
 import { Send } from "iconsax-react";
-import React from "react";
+import React, { useState } from "react";
 
 const Footer = () => {
+  const [form, setForm] = useState({
+    subject: "",
+    body: "",
+  });
   return (
     <Box
-    id="contact"
+      id="contact"
       px={{ lg: 100, md: "90px", sm: 10, base: 10 }}
       py={40}
       mt={{ lg: 100, md: "80px", sm: 0, base: 0 }}
@@ -31,30 +36,56 @@ const Footer = () => {
             Let me Handle your Next Project
           </Heading>
         </Box>
-        <Box width={{ lg: "30vw", md: "100%", sm: "100%", base : "100%" }} mt={"20px"}>
+        <Box
+          width={{ lg: "30vw", md: "100%", sm: "100%", base: "100%" }}
+          mt={"20px"}
+        >
           <FormControl>
-            <FormLabel>Email Address</FormLabel>
-            <Input width={"100%"} placeholder="you@gmail.com" fontSize={12} />
+            <FormLabel>Subject</FormLabel>
+            <Input
+              value={form.subject}
+              onChange={(e) => {
+                setForm({ ...form, subject: e.target.value });
+              }}
+              width={"100%"}
+              placeholder="Role of a Software Developer"
+              fontSize={12}
+            />
           </FormControl>
           <FormControl mt={5}>
             <FormLabel>Message</FormLabel>
-            <Textarea width={"100%"} placeholder="Message..." fontSize={12} />
+            <Textarea
+              value={form.body}
+              onChange={(e) => {
+                setForm({ ...form, body: e.target.value });
+              }}
+              width={"100%"}
+              placeholder="Message..."
+              fontSize={12}
+            />
           </FormControl>
-          <Button
-            rightIcon={<Send />}
-            bg={"tomato"}
-            color={"#fff"}
-            fontWeight={300}
-            width={"100%"}
-            mt={5}
-            py={7}
-            _hover={"none"}
+          <Link
+            href={`mailto:areegbedavid@gmail.com?subject=${form.subject}&body=${form.body}`}
           >
-            Send Message
-          </Button>
-          <Checkbox  pt={5}>
-            <Text fontSize={14}>I aggree to the Terms and Condition for Sending mail</Text>
-          </Checkbox>
+            <Button
+              rightIcon={<Send />}
+              bg={"tomato"}
+              color={"#fff"}
+              fontWeight={300}
+              width={"100%"}
+              mt={5}
+              py={7}
+              _hover={"none"}
+            >
+              Send Message
+            </Button>
+          </Link>
+          <Text pt={4} textAlign={"center"}>&copy; All rights reserved Areegbe David</Text>
+          {/* <Checkbox pt={5}>
+            <Text fontSize={14}>
+              I aggree to the Terms and Condition for Sending mail
+            </Text>
+          </Checkbox> */}
         </Box>
       </Flex>
     </Box>
